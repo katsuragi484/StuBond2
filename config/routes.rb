69e1fe0b92d5d015f59433fb759749e038742de6
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-07 04:53:55 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :parents
+  devise_for :students
+  devise_for :teachers
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
