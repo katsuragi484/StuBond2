dfe709c9b1cee8269ce14e53bc81928cd2517447
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-10 09:11:12 +0000

    [Add]student_report_index,show

--- app/controllers/student/reports_controller.rb
+++ app/controllers/student/reports_controller.rb
@@ -1,35 +1,11 @@
 class Student::ReportsController < ApplicationController
 
-  def new
-    @report = Report.new
-    @subjects = Subject.all
-  end
-
-  def create
-    report = Report.new(report_params)
-    report.save
-    redirect_to 'teacher_report_path(report.id)'
-  end
-
-  def index
-
+   def index
+    @reports = Report.all
   end
 
   def show
-
+    @report = Report.find(params[:id])
   end
 
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  private
-
-  def report_params
-    params.require(:report).permit(:title, :body, :subject_id)
-  end
 end
