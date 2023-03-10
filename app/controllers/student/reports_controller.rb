28ec21c33734dd3f4d15f35e54b21ab53381edeb
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-10 02:44:57 +0000

    [Add]teacher/reports/new_view

--- app/controllers/student/reports_controller.rb
+++ app/controllers/student/reports_controller.rb
@@ -1,2 +1,23 @@
 class Student::ReportsController < ApplicationController
+
+  def new
+    @report = Report.new
+    @subjects = Subject.all
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
