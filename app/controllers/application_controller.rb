3a8da9c1b13a6b62076c3d8278deb72f42bd32e7
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-13 07:40:03 +0000

    [Fix]teacher_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     new_admin_registration_path
   end
-  def after_sign_in_path_for(resource)
+  def after_sign_up_path_for(resource)
     new_admin_registration_path
   end
 
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:code, :email])
   end
+
 
 end
