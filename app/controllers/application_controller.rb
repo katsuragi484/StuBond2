6589260e7752b530ae8b9a453c3e4ff564f5aca0
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-14 03:22:00 +0000

    [Fix]ログアウト後画面遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,37 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    new_admin_registration_path
+    if resource_or_scope == :admin
+      new_admin_session_path
+    elsif resource_or_scope == :teacher
+      new_teacher_session_path
+    elsif resource_or_scope == :student
+      new_student_session_path
+    else
+      new_parent_session_path
+    end
   end
   def after_sign_up_path_for(resource)
-    new_admin_registration_path
+    if resource_or_scope == :admin
+      new_admin_session_path
+    elsif resource_or_scope == :teacher
+      new_teacher_session_path
+    elsif resource_or_scope == :student
+      new_student_session_path
+    else
+      new_parent_session_path
+    end
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    elsif resource_or_scope == :teacher
+      new_teacher_session_path
+    elsif resource_or_scope == :student
+      new_student_session_path
+    else
+      new_parent_session_path
+    end
   end
 
   protected
