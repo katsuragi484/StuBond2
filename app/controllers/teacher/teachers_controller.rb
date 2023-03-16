e98e4c2b773743920190e94f2c6d7558518310ab
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-16 03:50:49 +0000

    [Add]teacher_my_page

--- app/controllers/teacher/teachers_controller.rb
+++ app/controllers/teacher/teachers_controller.rb
@@ -1,2 +1,24 @@
 class Teacher::TeachersController < ApplicationController
+  def show
+    @teacher = current_teacher
+  end
+
+  def edit
+    @teacher = current_teacher
+  end
+
+  def update
+    @teacher = current_teacher
+    if @teacher.update(teacher_params)
+      redirect_to my_page_teacher_teachers_path
+    else
+      render :edit, notice: "変更に失敗しました"
+    end
+  end
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:is_deleted, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :word, :phone_number)
+  end
 end
