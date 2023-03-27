1e97a8edd86dd74dbe3efab023815c4e5b948547
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-03-27 11:08:55 +0000

    [Add]admin teacherのedit,update

--- app/controllers/admin/teachers_controller.rb
+++ app/controllers/admin/teachers_controller.rb
@@ -8,4 +8,23 @@ class Admin::TeachersController < ApplicationController
   def show
     @teacher = Teacher.find(params[:id])
   end
+
+  def edit
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def update
+    @teacher = Teacher.find(params[:id])
+    if @teacher.update(teacher_params)
+      redirect_to admin_teacher_path(@teacher.id)
+    else
+      render :edit, notice: "変更に失敗しました"
+    end
+  end
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:is_deleted, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :word, :phone_number, :code, :image)
+  end
 end
